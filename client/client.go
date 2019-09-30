316edd9f4e0bde739d1d21b1f4777f8784ba14a0
Vyacheslav Nichkov <v.nichkov@hotmail.com>
2019-09-30 16:18:01 +0600

    new crawler

---
+++ client/client.go
@@ -0,0 +1,104 @@
+package client
+
+import (
+	"log"
+	"math/rand"
+	"net/http"
+	"time"
+)
+
+type UserSlice struct {
+	arr []string
+}
+
+func (array *UserSlice)sample() string  {
+	rand.Seed(time.Now().Unix())
+	result :=array.arr[(rand.Int() % len(array.arr))]
+	return result
+}
+
+type HttpClient struct {
+	agent string
+	lang string
+	client http.Client
+}
+
+func (client *HttpClient) newRequest(url string) (*http.Request, error)   {
+	client.selectAgent()
+	client.selectLang()
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("User-Agent", client.agent)
+	req.Header.Set("Accept-Language", client.lang)
+
+	return req, nil
+
+}
+
+func (client *HttpClient) selectAgent()  {
+	if client.agent == "" {
+		log.Println("Зашли")
+		agents := UserSlice{[]string{
+			"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:64.0) Gecko/20100101 Firefox/64.0",
+			"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/600.1.25 (KHTML, like Gecko) Version/8.0 Safari/600.1.25",
+			"Mozilla/5.0 (X11; Ubuntu; Linux x86_64) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/11.0 Safari/605.1.15 Epiphany/605.1.15",
+			"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) snap Chromium/70.0.3538.110 Chrome/70.0.3538.110 Safari/537.36",
+			"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) snap Chromium/70.0.3538.110 Chrome/70.0.3538.110 Safari/537.36",
+			"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/602.1 (KHTML, like Gecko) Arora/0.11.0 Version/9.0 Safari/602.1",
+			"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) snap Chromium/70.0.3538.110 Chrome/70.0.3538.110 Safari/537.36",
+			"Mozilla/5.0 (X11; U; Linux i686; ru; rv:1.9b5) Gecko/2008050509 Firefox/3.0b5",
+			"Mozilla/5.0 (X11; U; Linux i686; ru; rv:1.9b5) Gecko/2008050509 Firefox/3.0b5",
+			"Mozilla/5.0 (Windows NT 6.1; WOW64; rv:63.0) Gecko/20100101 Firefox/63.0",
+			"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:64.0) Gecko/20100101 Firefox/64.0",
+		}}
+
+		client.agent = agents.sample()
+	}
+}
+
+func (client *HttpClient) selectLang()  {
+	if client.lang =="" {
+		langs := UserSlice{[]string{
+			"de-CH",
+			"en-US,en;q=0.5",
+			"fr-CH, fr;q=0.9, en;q=0.8, de;q=0.7, *;q=0.5",
+			"ru-RU, ru;q=0.9, en-US;q=0.8, en;q=0.7, fr;q=0.6",
+		}}
+		client.lang = langs.sample()
+	}
+}
+
+func (client *HttpClient) Agent() string  {
+	return client.agent
+}
+
+func (client *HttpClient) Get(url string) (*http.Response,  error) {
+
+	req, err := client.newRequest(url)
+	if err != nil {
+		panic(err)
+	}
+
+	if &client.client == nil {
+		client.client = http.Client{}
+	}
+	resp, err := client.client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	//defer resp.Body.Close()
+
+	/*responseData,err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	res := string(responseData)*/
+
+	return resp, nil
+
+}
--- run.go
+++ run.go
@@ -1,11 +1,193 @@
 package main
 
-import "github.com/go-martini/martini"
+import (
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"net/url"
+	"proxy/client"
+)
+
+type user_arr struct {
+	elem map[string]bool
+}
+
+func (u *user_arr) new()  {
+	if len(u.elem) == 0 {
+		origin_arr := []string{
+			"class",
+			"href",
+			"id",
+		}
+		u.elem = make(map[string]bool)
+		for _, t :=range origin_arr{
+			u.elem[t] = true
+		}
+	}
+}
+
+var u user_arr
+
+func (u *user_arr) includes(t string) bool{
+	return u.elem[t]
+}
+
+func _check(err error) {
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+}
+
+func _check_err(err error) int {
+	if err != nil {
+		return 500
+	}
+	return 200
+}
+
+func changeQuery(query url.Values) string {
+	query.Set("tracking", "jobsgalore")
+	query.Set("utm_source", "jobsgaloreeu")
+	query.Set("utm_campaign", "jobsgaloreeu")
+	query.Set("utm_medium", "organic")
+	return query.Encode()
+}
+
+func updateUrl(url_path string) string{
+	log.Println( "Старый урл " + url_path)
+	result := url_path
+	u, err := url.Parse(url_path)
+	_check(err)
+
+	if u != nil {
+		u.RawQuery = changeQuery(u.Query())
+		result = u.String()
+	}
+
+	log.Println( "Новый урл " + result)
+
+	return result
+
+}
+
+
 
 func main() {
-	m := martini.Classic()
-	m.Get("/", func() string {
-		return "Hello World"
+
+	httpClient := client.HttpClient{}
+
+	m := gin.Default()
+	m.GET("/", func(c *gin.Context)  {
+		c.String(http.StatusOK, "Working")
 	})
-	m.Run()
+
+	m.GET("/open", func(c *gin.Context) {
+		url := c.Query("url")
+		result :=""
+		if url !="" {
+			response, err := httpClient.Get(url)
+			log.Println(fmt.Sprintf("url: \"%s  | agent: \"%s", url, httpClient.Agent()))
+			doc, err := goquery.NewDocumentFromResponse(response)
+			_check(err)
+
+			u := user_arr{}
+			u.new()
+
+			body := doc.Find("div.search-results-container, div.job_info, td#resultsCol, " +
+				"div.jobsearch-JobComponent, div#jobResults").Each(func(i int, selection *goquery.Selection) {
+				n := selection.Nodes
+				for _, elem := range n {
+					for _, at :=range elem.Attr{
+						if !u.includes(at.Key){
+							selection.RemoveAttr(at.Key)
+						}
+					}
+				}
+			})
+			body.Find("svg, link, script, style").Remove()
+
+			//removeExcess(body)
+			result,_ = body.Html()
+
+		}
+		//c.String(http.StatusOK, result)
+		c.Data(http.StatusOK, "text/html; charset=utf-8",[]byte(result))
+
+	})
+
+	m.GET("/redirect", func(c *gin.Context) {
+
+		origin_url := c.Query("url")
+		//aplitrak  := "www.aplitrak.com"
+		status :=200
+		res := ""
+		if origin_url !="" {
+
+			//Получаем страницу по url
+			response, err := httpClient.Get(origin_url)
+			log.Println(fmt.Sprintf("url: \"%s  | agent: \"%s", origin_url, httpClient.Agent()))
+
+			status =_check_err(err)
+
+			//Если получили страницу, то продолжаем иначе у оригинального URL меняем метки и выходим
+			if status==200 {
+				/*new_url := ""
+				//Если ссылается на аплитрак, то получаем с него ссылку
+				if aplitrak == response.Request.URL.Host {
+					log.Println("Aplitrak url: " + response.Request.URL.String())
+					_, err := goquery.NewDocumentFromResponse(response)
+					flag := _check_err(err)
+					if flag == 200 {
+						new_url = response.Request.URL.String()
+						/*body := doc.Find("div.search-results-container, div.job_info, td#resultsCol, " +
+							"div.jobsearch-JobComponent, div#jobResults")
+						newUrl :=
+					} else {
+						new_url = response.Request.URL.String()
+					}
+				} else{
+					log.Println("Не литрак url: " + response.Request.URL.String())
+					new_url = response.Request.URL.String()
+				}
+
+				//Проверяем полученный URL
+				respChecking, err := httpClient.Get(new_url)
+				_check(err)
+
+				if respChecking.StatusCode == 200{
+					log.Println("Ссылка работает : " + new_url)
+					res = new_url
+				} else {
+					log.Println(fmt.Sprintf("url: \"%s  | agent: \"%s", new_url, httpClient.Agent()))
+					log.Println("Ссылка не аботает : " + new_url)
+					log.Println(respChecking.StatusCode)
+					res = origin_url
+				}
+				res = new_url
+				*/
+
+				res = response.Request.URL.String()
+			} else {
+				res = origin_url
+			}
+
+			res = updateUrl(res)
+			//Вставляем в  URL свои метки
+
+
+		}
+
+		c.PureJSON(status, gin.H{
+			"uri":res,
+			"body": nil,
+		})
+	})
+
+
+
+
+
+	m.Run(":3000")
 }
